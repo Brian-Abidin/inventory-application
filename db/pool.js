91df9d79ecd9086bcbe82757a80ed2f81ed2cb1d
Brian Abidin <Brian.abidin1@gmail.com>
2025-08-22 13:23:49 -0400

    fixed spelling error with database

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 module.exports = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  databse: process.env.DB_NAME,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT
 });
