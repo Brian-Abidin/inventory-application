5aa19c0119377c2a2e105f66570d2455d8a432b5
Brian Abidin <Brian.abidin1@gmail.com>
2025-09-22 15:40:16 -0400

    updated query in getAllData

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require("./pool");
 
 async function getAllData() {
-  const { rows } = await pool.query("SELECT * FROM usernames");
+  const { rows } = await pool.query("SELECT * FROM games");
   return rows;
 }
 
