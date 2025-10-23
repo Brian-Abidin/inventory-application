f86ff8304e43d34523e8af912e4e64f941a78cfc
Brian Abidin <Brian.abidin1@gmail.com>
2025-10-23 13:55:37 -0400

    added router for categories page

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,6 @@ const usersRouter = Router();
 
 usersRouter.get("/", usersController.getIndex);
 usersRouter.get("/games", usersController.getGames);
+usersRouter.get("/categories", usersController.getCategories);
 
 module.exports = usersRouter;
