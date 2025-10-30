a57c449361c4d22e8f285cf43a5e10f6e2137108
Brian Abidin <Brian.abidin1@gmail.com>
2025-10-30 14:18:05 -0400

    added route to games details page

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,5 +6,6 @@ const usersRouter = Router();
 usersRouter.get("/", usersController.getIndex);
 usersRouter.get("/games", usersController.getGames);
 usersRouter.get("/categories", usersController.getCategories);
+usersRouter.get("/games/:query", usersController.getGameDetails);
 
 module.exports = usersRouter;
