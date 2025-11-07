8398b57559f59cda922cad455972fcfe151a86c8
Brian Abidin <Brian.abidin1@gmail.com>
2025-11-07 17:13:11 -0500

    added route for search view

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,5 +7,6 @@ usersRouter.get("/", usersController.getIndex);
 usersRouter.get("/games", usersController.getGames);
 usersRouter.get("/categories", usersController.getCategories);
 usersRouter.get("/games/:id", usersController.getGameDetails);
+usersRouter.get("/search", usersController.getSearch);
 
 module.exports = usersRouter;
