96dedd1ec66171a24899dca26aebb589c0c68b55
Brian Abidin <Brian.abidin1@gmail.com>
2025-12-13 00:13:12 -0500

    removed old table and added 5 new tables that relate to each other

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,18 +1,64 @@
 #! /usr/bin/env node
 
 const { Client } = require("pg");
+require("dotenv").config();
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    game VARCHAR ( 255 ),
-    developers VARCHAR ( 255 )
+    name VARCHAR ( 255 ),
+    description TEXT
 );
 
-INSERT INTO games (game, developers)
+CREATE TABLE IF NOT EXISTS devs (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS games_devs (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES games (id),
+    dev_id INTEGER REFERENCES devs (id)
+);
+
+CREATE TABLE IF NOT EXISTS games_genres (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES games (id),
+    genre_id INTEGER REFERENCES genres (id)
+);
+
+INSERT INTO games (name, description)
 VALUES
-    ('Overwatch 2', 'Blizzard Entertainment'),
-    ('Call of Duty 6', 'Treyarch, Raven Software');
+    ('Overwatch 2', 'Overwatch 2 is a 2023 American first-person shooter video game by Blizzard Entertainment. As a sequel and replacement to the 2016 hero shooter Overwatch, the game includes new game modes and a reduction in team size from six to five.'),
+    ('Call of Duty 6', 'Call of Duty: Black Ops 6 is a 2024 first-person shooter video game co-developed by Treyarch and Raven Software and published by Activision. It is the twenty-first installment of the Call of Duty series and is the seventh main entry in the Black Ops sub-series, following Call of Duty: Black Ops Cold War (2020).');
+
+INSERT INTO devs (name)
+VALUES
+    ('Blizzard Entertainment'),
+    ('Treyarch'),
+    ('Raven Software');
+
+INSERT INTO genres (name)
+VALUES
+    ('FPS'),
+    ('Hero Shooter');
+
+INSERT INTO games_devs (game_id, dev_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (2, 3);
+
+INSERT INTO games_genres (game_id, genre_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1);
 `;
 
 async function main() {
