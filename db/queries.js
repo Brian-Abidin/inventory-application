df12a3d9deaf10f74e20fe69285f03b1f80f2bc6
Brian Abidin <Brian.abidin1@gmail.com>
2025-12-16 15:10:09 -0500

    commented out all functions except getAllData and updated the query for getAllData

--- db/queries.js
+++ db/queries.js
@@ -1,35 +1,37 @@
 const pool = require("./pool");
 
 async function getAllData() {
-  const { rows } = await pool.query("SELECT * FROM games");
-  return rows;
-}
-
-async function insertGame(game) {
-  await pool.query("INSERT INTO games (game) VALUES ($1)", [game]);
-}
-
-async function getGameInfoById(id) {
-  const { rows } = await pool.query(`SELECT * FROM games WHERE id = ${id}`);
-  return rows;
-}
-
-async function searchGamesByTitle(searchTerm) {
   const { rows } = await pool.query(
-    `SELECT * FROM games WHERE game ILIKE ('%${searchTerm}%')`
+    "SELECT games.id AS game_id, games.name, games.description, devs.name AS dev, genres.name AS genre FROM games INNER JOIN games_devs ON games.id = games_devs.game_id INNER JOIN devs ON games_devs.dev_id = devs.id INNER JOIN games_genres ON games.id = games_genres.game_id INNER JOIN genres ON games_genres.genre_id = genres.id"
   );
   return rows;
 }
 
-async function updateGameDetails(name, devs, genre, desc, id) {
-  await pool.query(
-    `UPDATE games SET game = ${name}, developers = ${devs}, genre = ${genre}, description = ${desc} WHERE id = ${id} `
-  );
-}
+// async function insertGame(game) {
+//   await pool.query("INSERT INTO games (game) VALUES ($1)", [game]);
+// }
 
-async function deleteGameById(id) {
-  await pool.query(`DELETE FROM games WHERE id = ${id}`);
-}
+// async function getGameInfoById(id) {
+//   const { rows } = await pool.query(`SELECT * FROM games WHERE id = ${id}`);
+//   return rows;
+// }
+
+// async function searchGamesByTitle(searchTerm) {
+//   const { rows } = await pool.query(
+//     `SELECT * FROM games WHERE game ILIKE ('%${searchTerm}%')`
+//   );
+//   return rows;
+// }
+
+// async function updateGameDetails(name, devs, genre, desc, id) {
+//   await pool.query(
+//     `UPDATE games SET game = ${name}, developers = ${devs}, genre = ${genre}, description = ${desc} WHERE id = ${id} `
+//   );
+// }
+
+// async function deleteGameById(id) {
+//   await pool.query(`DELETE FROM games WHERE id = ${id}`);
+// }
 
 // async function searchGamesByGenre(genresArr) {
 //   console.log(genresArr.map((genre) => `'${genre}'`).join(", "), 2);
@@ -42,12 +44,12 @@ async function deleteGameById(id) {
 // }
 
 module.exports = {
-  getAllData,
-  getGameInfoById,
-  searchGamesByTitle,
-  updateGameDetails,
-  deleteGameById,
-  searchGamesByGenre
+  getAllData
+  // getGameInfoById,
+  // searchGamesByTitle,
+  // updateGameDetails,
+  // deleteGameById,
+  // searchGamesByGenre
 };
 
 // NEED TO UPDATE FUNCTIONS DUE TO CHANGING RELATIONAL TABLES
