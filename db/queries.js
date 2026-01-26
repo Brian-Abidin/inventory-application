952db7a81c92966999c11e9ca565db3a34c69fbc
Brian Abidin <Brian.abidin1@gmail.com>
2026-01-26 15:16:55 -0500

    added commeent with example queries

--- db/queries.js
+++ db/queries.js
@@ -7,6 +7,46 @@ async function getAllData() {
   return rows;
 }
 
+async function updateGame(name, devs, genre, desc, id) {
+  await pool.query(
+    `UPDATE games SET game = ${name}, developers = ${devs}, genre = ${genre}, description = ${desc} WHERE id = ${id} `
+  );
+}
+
+async function updateDevs(devs) {}
 module.exports = {
   getAllData
 };
+
+/*
+Example queries to update game and relational tables
+
+UPDATE games SET name = 'Highguard' WHERE id = 1;
+
+INSERT INTO devs (name) VALUES ('Wildlight Entertainment') ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO genres (name) VALUES ('Hero Shooter') ON CONFLICT (name) DO NOTHING;
+
+DELETE FROM games_devs WHERE game_id = 1;
+
+INSERT INTO games_devs (game_id, dev_id) WITH
+t1 AS (
+  SELECT id FROM games WHERE name = 'Highguard'
+  ),
+ t2 AS (
+   SELECT id FROM devs WHERE name = 'Wildlight Entertainment'
+   )
+ SELECT t1.id, t2.id FROM t1, t2;
+ 
+ DELETE FROM games_genres WHERE game_id = 1;
+ 
+ INSERT INTO games_genres (game_id, genre_id) WITH
+t1 AS (
+  SELECT id FROM games WHERE name = 'Highguard'
+  ),
+ t2 AS (
+   SELECT id FROM genres WHERE name = 'Hero Shooter'
+   )
+  SELECT t1.id, t2.id FROM t1, t2;
+
+*/
