acc43655f30f1aa820c199defdffe26cf4361ebd
Brian Abidin <Brian.abidin1@gmail.com>
2026-02-03 00:19:43 -0500

    added more comments

--- db/queries.js
+++ db/queries.js
@@ -21,7 +21,71 @@ module.exports = {
 /*
 Example queries to update game and relational tables
 
+CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ),
+    description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS devs (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 )
+);
+
+CREATE TABLE IF NOT EXISTS games_devs (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES games (id),
+    dev_id INTEGER REFERENCES devs (id)
+);
+
+CREATE TABLE IF NOT EXISTS games_genres (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER REFERENCES games (id),
+    genre_id INTEGER REFERENCES genres (id)
+);
+
+INSERT INTO games (name, description)
+VALUES
+    ('Overwatch 2', 'Overwatch 2 is a 2023 American first-person shooter video game by Blizzard Entertainment. As a sequel and replacement to the 2016 hero shooter Overwatch, the game includes new game modes and a reduction in team size from six to five.'),
+    ('Call of Duty 6', 'Call of Duty: Black Ops 6 is a 2024 first-person shooter video game co-developed by Treyarch and Raven Software and published by Activision. It is the twenty-first installment of the Call of Duty series and is the seventh main entry in the Black Ops sub-series, following Call of Duty: Black Ops Cold War (2020).');
+
+INSERT INTO devs (name)
+VALUES
+    ('Blizzard Entertainment'),
+    ('Treyarch'),
+    ('Raven Software');
+
+INSERT INTO genres (name)
+VALUES
+    ('FPS'),
+    ('Hero Shooter');
+
+INSERT INTO games_devs (game_id, dev_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (2, 3);
+
+INSERT INTO games_genres (game_id, genre_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1);
+    
+ ALTER TABLE games ADD CONSTRAINT unique_name UNIQUE (name);
+     
+ ALTER TABLE devs ADD CONSTRAINT unique_dev UNIQUE (name);
+     
+ ALTER TABLE genres ADD CONSTRAINT unique_genre UNIQUE (name);
+ 
 UPDATE games SET name = 'Highguard' WHERE id = 1;
+
+UPDATE games SET description = 'Highguard is a player versus player (PvP) raid shooter where players take on the roles of "Wardens", characters described as arcane gunslingers who fight for control over a mythical continent.' WHERE id = 1;
 
 INSERT INTO devs (name) VALUES ('Wildlight Entertainment') ON CONFLICT (name) DO NOTHING;
 
@@ -48,5 +112,5 @@ t1 AS (
    SELECT id FROM genres WHERE name = 'Hero Shooter'
    )
   SELECT t1.id, t2.id FROM t1, t2;
-
+ 
 */
