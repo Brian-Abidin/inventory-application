592b8a5cc348e7c20a61266bae41dd4a58dae0bb
Brian Abidin <Brian.abidin1@gmail.com>
2026-02-04 23:40:38 -0500

    updated updateGamesTable. Now update name and description of games table

--- db/queries.js
+++ db/queries.js
@@ -7,81 +7,29 @@ async function getAllData() {
   return rows;
 }
 
-async function updateGame(name, devs, genre, desc, id) {
+async function updateGamesTable(name, description, id) {
+  console.log(name, description, id);
   await pool.query(
-    `UPDATE games SET game = ${name}, developers = ${devs}, genre = ${genre}, description = ${desc} WHERE id = ${id} `
+    "UPDATE games SET name = $1, description = $2 WHERE id = $3",
+    [name, description, id]
   );
 }
 
-async function updateDevs(devs) {}
+async function updateDevsTable(devs, id) {}
+
 module.exports = {
-  getAllData
+  getAllData,
+  updateGamesTable
 };
 
 /*
 Example queries to update game and relational tables
-
-CREATE TABLE IF NOT EXISTS games (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR ( 255 ),
-    description TEXT
-);
-
-CREATE TABLE IF NOT EXISTS devs (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR ( 255 )
-);
-
-CREATE TABLE IF NOT EXISTS genres (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR ( 255 )
-);
-
-CREATE TABLE IF NOT EXISTS games_devs (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    game_id INTEGER REFERENCES games (id),
-    dev_id INTEGER REFERENCES devs (id)
-);
-
-CREATE TABLE IF NOT EXISTS games_genres (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    game_id INTEGER REFERENCES games (id),
-    genre_id INTEGER REFERENCES genres (id)
-);
-
-INSERT INTO games (name, description)
-VALUES
-    ('Overwatch 2', 'Overwatch 2 is a 2023 American first-person shooter video game by Blizzard Entertainment. As a sequel and replacement to the 2016 hero shooter Overwatch, the game includes new game modes and a reduction in team size from six to five.'),
-    ('Call of Duty 6', 'Call of Duty: Black Ops 6 is a 2024 first-person shooter video game co-developed by Treyarch and Raven Software and published by Activision. It is the twenty-first installment of the Call of Duty series and is the seventh main entry in the Black Ops sub-series, following Call of Duty: Black Ops Cold War (2020).');
-
-INSERT INTO devs (name)
-VALUES
-    ('Blizzard Entertainment'),
-    ('Treyarch'),
-    ('Raven Software');
-
-INSERT INTO genres (name)
-VALUES
-    ('FPS'),
-    ('Hero Shooter');
-
-INSERT INTO games_devs (game_id, dev_id)
-VALUES
-    (1, 1),
-    (2, 2),
-    (2, 3);
-
-INSERT INTO games_genres (game_id, genre_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (2, 1);
-    
- ALTER TABLE games ADD CONSTRAINT unique_name UNIQUE (name);
+  
+ALTER TABLE games ADD CONSTRAINT unique_name UNIQUE (name);
      
- ALTER TABLE devs ADD CONSTRAINT unique_dev UNIQUE (name);
+ALTER TABLE devs ADD CONSTRAINT unique_dev UNIQUE (name);
      
- ALTER TABLE genres ADD CONSTRAINT unique_genre UNIQUE (name);
+ALTER TABLE genres ADD CONSTRAINT unique_genre UNIQUE (name);
  
 UPDATE games SET name = 'Highguard' WHERE id = 1;
 
